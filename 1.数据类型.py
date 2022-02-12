9a34845ba72da20edbc28adede36be32b35b6f72
Rvran <1044232458@qq.com>
2022-02-12 23:16:47 +0800

    Python Study

--- 1.数据类型.py
+++ 1.数据类型.py
@@ -1,5 +1,5 @@
-#Python学习笔记-数据类型
-#单行注释
+# Python学习笔记-数据类型
+# 单行注释
 """
 多
 行
@@ -7,24 +7,25 @@
 释
 """
 
-#浮点运算时为避免小概率出错，引用decimal
+# 浮点运算时为避免小概率出错，引用decimal
 from decimal import Decimal
+
 print(Decimal('1.1') + Decimal('2.2'))
 
-#数据类型-Boolean
+# 数据类型-Boolean
 num = True
 num2 = False
 print(num, type(num))
 print(num2, type(num2))
 
-#数据类型转换
+# 数据类型转换
 name = '然'
 age = 23
-#name（str）和age（int）不是同一种数据类型，需要将age转换为str数据类型str（age）才能正常拼接
-#否则会报错 TypeError: can only concatenate str (not "int") to str
-print('我叫'+name+'，今年'+str(age)+'岁了')
+# name（str）和age（int）不是同一种数据类型，需要将age转换为str数据类型str（age）才能正常拼接
+# 否则会报错 TypeError: can only concatenate str (not "int") to str
+print('我叫' + name + '，今年' + str(age) + '岁了')
 
-#将其他类型转换为int类型
+# 将其他类型转换为int类型
 print('-------将其他数据类型转换为int类型-----------------')
 s1 = 128
 f1 = 98.7
@@ -32,12 +33,12 @@ s2 = '76.77'
 ff = True
 s3 = 'hello'
 print(type(s1), type(f1), type(s2), type(ff), type(s3))
-print(int(s1), type(int(s1)))   #将str转成int类型，字符串为数字串
-print(int(f1), type(int(f1)))   #将float转成int类型，舍弃小数部分，取整数
-print(int(ff), type(int(ff)))   #将Boolean转换为int，Ture→1，False→0
+print(int(s1), type(int(s1)))  # 将str转成int类型，字符串为数字串
+print(int(f1), type(int(f1)))  # 将float转成int类型，舍弃小数部分，取整数
+print(int(ff), type(int(ff)))  # 将Boolean转换为int，Ture→1，False→0
 
-#将其他类型转换为float类型
+# 将其他类型转换为float类型
 print('-------将其他数据类型转换为float类型-----------------')
 
-#int类型转换为float类型，默认保留小数点一位
+# int类型转换为float类型，默认保留小数点一位
 print(float(s1), type(float(s1)))
--- 2.input函数.py
+++ 2.input函数.py
@@ -1,11 +1,11 @@
-#Python学习笔记-input函数
+# Python学习笔记-input函数
 
-#输入函数input
+# 输入函数input
 age = input('你今年几岁了？\n')
 print(age, type(age))
 
-#输入两个数，算和
+# 输入两个数，算和
 num1 = input('输入第一个数：')
 num2 = input('输入第二个数：')
-result = float(num1)+float(num2)  #注意input的数据类型，默认为str不能直接相加
+result = float(num1) + float(num2)  # 注意input的数据类型，默认为str不能直接相加
 print('和为：', result)
